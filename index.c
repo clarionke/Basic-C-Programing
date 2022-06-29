c2a31473d99d417dbfae77277236431cda56faac
clarionke <104459747+clarionke@users.noreply.github.com>
2022-06-29 09:57:40 +0800

    C printf function

--- index.c
+++ index.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    char characterName[] = "kamau";
+   /** char characterName[] = "kamau";
     int characterAge = 85;
     printf("There once was a man named %s\n", characterName);
     printf("he was %d years old.\n", characterAge);
@@ -11,7 +11,8 @@ int main()
     characterAge = 25;
 
     printf("He really liked the name %s\n",characterName);
-    printf("but did not like being %d.\n", characterAge);
+    printf("but did not like being %d.\n", characterAge);**/
+    printf("My favorite %s is %d","Number", 12);
 
     return 0;
 }
---
+++ printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int learn()
+{
+    printf("This is my favorite %s is %d","Number", 12);
+
+    return 0;
+
+}

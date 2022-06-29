8ff80fd66a474f460a14f702a7bc93e43a525ff9
clarionke <104459747+clarionke@users.noreply.github.com>
2022-06-29 17:19:35 +0800

    Building a basic calculator

--- index.c
+++ index.c
@@ -30,10 +30,18 @@ int main()
    scanf("%d", &age);
    printf("Your are %d Years old", age);
    **/
-   char name[20];
+   /**char name[20];
    printf("Enter Your name: ");
    fgets(name,30, stdin);
-   printf("Your name is %s", name);
+   printf("Your name is %s", name);**/
+
+   double num1;
+   double num2;
+   printf("Enter the first number: ");
+   scanf("%lf", &num1);
+   printf("Enter the second number: ");
+   scanf("%lf", &num2);
+   printf("Answer: %f", num1 + num2);
 
     return 0;
 }
